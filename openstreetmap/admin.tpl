e0cd621f8324d10fbf4cb735d7ab7d2a63fc4c4f
Klaus Weidenbach <Klaus.Weidenbach@gmx.net>
2012-02-25 23:18:53 +0100

    Test for HTML5 enabled field_number form input element.
    Testing if key => value pairs work and the number input element feature in general.

--- openstreetmap/admin.tpl
+++ openstreetmap/admin.tpl
@@ -1,3 +1,3 @@
 {{ inc field_input.tpl with $field=$tmsserver }}{{ endinc }}
-{{ inc field_input.tpl with $field=$zoom }}{{ endinc }}
+{{ inc field_number.tpl with $field=$zoom }}{{ endinc }}
 <div class="submit"><input type="submit" name="page_site" value="$submit" /></div>
--- openstreetmap/openstreetmap.php
+++ openstreetmap/openstreetmap.php
@@ -69,7 +69,7 @@ function openstreetmap_plugin_admin (&$a, &$o) {
 	$o = replace_macros( $t, array(
 		'$submit' => t('Submit'),
 		'$tmsserver' => array('tmsserver', t('Tile Server URL'), $tmsserver, t('A list of <a href="http://wiki.openstreetmap.org/wiki/TMS" target="_blank">public tile servers</a>')),
-		'$zoom' => array('zoom', t('Default zoom'), $zoom, t('The default zoom level. (1:world, 18:highest)')),
+		'$zoom' => array('zoom', t('Default zoom'), $zoom, t('The default zoom level. (1:world, 18:highest)'), 'required'=>'1', 'min'=>'1', 'max'=>'18'),
 	));
 }
 function openstreetmap_plugin_admin_post (&$a) {
